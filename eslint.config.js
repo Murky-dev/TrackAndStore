03a26d7f81d085512299769d5061c8bda27af04b
Connor Youngs <connormyoungs@gmail.com>
2025-12-01 16:44:12 +0000

    Added prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
 
 export default defineConfig([
   { ignores: ["./postgres-data"] },
@@ -9,4 +11,5 @@ export default defineConfig([
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  eslintConfigPrettier
 ]);
--- src/service/db.js
+++ src/service/db.js
@@ -1,4 +1,4 @@
 import postgres from "postgres";
-import dotenv from "dotenv/config"; // eslint-disable-line 
+import dotenv from "dotenv/config"; // eslint-disable-line
 const sql = postgres(process.env.DATABASE);
 export default sql;
