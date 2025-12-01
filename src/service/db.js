de6283281fbaa4bcb4a47c0bcce641f967d86c9e
Connor Youngs <connormyoungs@gmail.com>
2025-12-01 16:26:37 +0000

    Added eslint support

--- src/service/db.js
+++ src/service/db.js
@@ -1,5 +1,4 @@
 import postgres from "postgres";
-import dotenv from "dotenv/config";
+import dotenv from "dotenv/config"; // eslint-disable-line 
 const sql = postgres(process.env.DATABASE);
-
 export default sql;
